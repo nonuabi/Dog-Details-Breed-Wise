0d8d343f941a314fac23e53a9bee9381278b9e54
nonuabi <nonuabi@gmail.com>
2021-04-05 08:05:20 +0530

    fetch button is working now!

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,6 +1,6 @@
 function getImage() {
   $.get("https://dog.ceo/api/breeds/list/all", function (data) {
-    var imageUrl = data.message.bulldog[1];
+    // var imageUrl = data.message.bulldog[1];
   });
 }
 
